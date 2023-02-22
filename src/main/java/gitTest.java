3c80e31de74731778dc7f7497197930ab07d11a0
hds <hds@fanzhou.com>
2023-02-22 13:34:13 +0800

    new one

--- src/main/java/gitTest.java
+++ src/main/java/gitTest.java
@@ -5,7 +5,7 @@ public class gitTest {
         System.out.println("hot-fix");
         System.out.println("master");
         System.out.println("hot-fix-again");
-        System.out.println("push-test");
+        System.out.println("push-test2");
     }
 }
 
