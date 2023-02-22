de976e2d23aa26bb38822b9eeaf6281eade44dd6
Bububbi <39119502+Bububbi@users.noreply.github.com>
2023-02-22 13:35:21 +0800

    删除了一部分
    测试

--- src/main/java/gitTest.java
+++ src/main/java/gitTest.java
@@ -1,11 +1,7 @@
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hot-fix");
-        System.out.println("master");
-        System.out.println("hot-fix-again");
-        System.out.println("push-test2");
+        
     }
 }
 
