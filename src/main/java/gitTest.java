5fffd13bfa197ad8abe7ab6ce6b0bb5c0e9d1e06
hds <hds@fanzhou.com>
2023-02-22 10:08:42 +0800

    push commit

--- src/main/java/gitTest.java
+++ src/main/java/gitTest.java
@@ -5,6 +5,7 @@ public class gitTest {
         System.out.println("hot-fix");
         System.out.println("master");
         System.out.println("hot-fix-again");
+        System.out.println("push-test");
     }
 }
 
