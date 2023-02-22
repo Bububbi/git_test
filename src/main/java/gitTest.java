56f5a924e76a362064fc5217b552bc985d0c43bf
hds <hds@fanzhou.com>
2023-02-22 10:08:23 +0800

    hot-fix commit

--- src/main/java/gitTest.java
+++ src/main/java/gitTest.java
@@ -3,6 +3,8 @@ public class gitTest {
         System.out.println("hello");
         System.out.println("hello");
         System.out.println("hot-fix");
+        System.out.println("master");
+
     }
 }
 
